e1f32f612104cea4c6a401e2b9af0cf4da0ab666
Abril <basteiroabril@gmail.com>
2021-10-19 12:34:47 -0300

    JavaScript fundamentals

---
+++ Fundamentals-1/script.js
@@ -0,0 +1,46 @@
+// Hello World! 
+
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Abril"
+console.log(firstName);
+
+// Values and Variables
+
+const country = "Argentina"
+let continent = "América del Sur"
+let population = 43000000
+
+console.log(country, continent, population);
+
+// Data Types
+let isIsland = false
+let language
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// let, const and var
+language = "spanish"
+
+// Basic Operators
+halfPopulation = population / 2
+console.log(halfPopulation);
+
+population++;
+console.log(population);
+
+finlandPop = 6000000
+console.log(population > finlandPop); 
+
+averagePop = 33000000
+console.log(population < averagePop);
+
+description = country + " is in " + continent + ", and it's " + population + " people speak " + language
+console.log(description);
+
