3312987c70d022b64815463a1b7e03e7f1e5f7b7
Abril <basteiroabril@gmail.com>
2021-10-19 17:15:05 -0300

    Type Conversion and Coercion

--- Fundamentals-1/challenge_2.js
+++ Fundamentals-1/challenge_2.js
@@ -15,5 +15,5 @@ console.log("Is Mark's higher than John's? " + markHigherBMI);
 if (markBMI > johnBMI){
     console.log(`"Mark's BMI (${markBMI.toFixed(2)}) is higher than John's (${johnBMI.toFixed(2)})!`)
 } else {
-    console.log(`"John's BMI (${johnBMI.toFixed(2)}) is higher than Mark's (${markBMI.toFixed(2)})!`)
+    console.log(`John's BMI (${johnBMI.toFixed(2)}) is higher than Mark's (${markBMI.toFixed(2)})!`)
 }
--- Fundamentals-1/script.js
+++ Fundamentals-1/script.js
@@ -55,3 +55,10 @@ if (population > averagePop){
 } else {
     console.log(`${country}'s population is ${averagePop - population} bellow average`)
 };
+
+// Type Conversion and Coercion
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
