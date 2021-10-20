a5189f6c54085624fc511df26bb5bb25d1c8d68d
Abril <basteiroabril@gmail.com>
2021-10-20 11:00:48 -0300

    Switch Statement

--- Fundamentals-1/script.js
+++ Fundamentals-1/script.js
@@ -75,9 +75,31 @@ console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
 // }
 
 // Logical Operators
-
 if(language === "english" && population < 50000000 && !isIsland){
     console.log(`You should live in ${country} :)`);
 } else {
     console.log(`${country} does not meet your criteria :(`);
+}
+
+// The switch Statement
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+        break;
 }
