3ab8ef9166d98c61b024b2b6d8a2078bd7e89494
Abril <basteiroabril@gmail.com>
2021-10-20 21:44:02 -0300

    Function Declarations vs. Expressions

--- Fundamentals-2/script.js
+++ Fundamentals-2/script.js
@@ -13,3 +13,32 @@ console.log(uruguay);
 
 const peru = describeCountry("Peru", 32, "Lima");
 console.log(peru);
+
+// Function Declarations vs. Expressions
+// Declaration
+function percentageOfWorld1 (population) {
+    return ((population / 7900) * 100).toFixed(2);
+}
+
+const china = percentageOfWorld1(1441);
+console.log(china);
+
+const jamaica = percentageOfWorld1(3);
+console.log(jamaica);
+
+const turquia = percentageOfWorld1(84);
+console.log(turquia);
+
+// Expression
+const percentageOfWorld2 = function (population) {
+    return ((population / 7900) * 100).toFixed(2);
+}
+
+const inglaterra = percentageOfWorld2(55);
+console.log(inglaterra);
+
+const alemania = percentageOfWorld2(83);
+console.log(alemania);
+
+const newZeland = percentageOfWorld2(5);
+console.log(newZeland);
