8d3449934b1865b4f34621b6ea3de779064a1301
Abril <basteiroabril@gmail.com>
2021-10-21 13:47:46 -0300

    2nd challenge

---
+++ Fundamentals-2/challenge_2.js
@@ -0,0 +1,20 @@
+
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * .15 : bill * .20; 
+}
+console.log(calcTip(100))
+
+const bills = [125, 55, 44]
+const tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2]),
+]
+console.log(tips)
+
+const totals = [
+    bills[0] + tips [0],
+    bills[1] + tips [1],
+    bills[2] + tips [2],
+]
+console.log(totals)
